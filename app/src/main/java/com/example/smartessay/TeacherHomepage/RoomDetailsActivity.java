cffeaa1c3eb148c26e203386455f51e046fbddb5
Khent <you@example.com>
2025-08-20 02:54:28 +0800

    Added Student Recycler View

--- app/src/main/java/com/example/smartessay/TeacherHomepage/RoomDetailsActivity.java
+++ app/src/main/java/com/example/smartessay/TeacherHomepage/RoomDetailsActivity.java
@@ -1,16 +1,25 @@
 package com.example.smartessay.TeacherHomepage;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartessay.R;
-import com.example.smartessay.Teacher_Fragments.HomeFragment_Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomDetailsActivity extends AppCompatActivity {
 
     TextView tvRoomName, tvRoomCode;
+    RecyclerView rvStudents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,7 @@ public class RoomDetailsActivity extends AppCompatActivity {
 
         tvRoomName = findViewById(R.id.tvRoomName);
         tvRoomCode = findViewById(R.id.tvRoomCode);
+        rvStudents = findViewById(R.id.recycler_view_rooms); // Use RecyclerView ID, not TextView ID
 
         String roomName = getIntent().getStringExtra("roomName");
         String roomCode = getIntent().getStringExtra("roomCode");
@@ -26,9 +36,53 @@ public class RoomDetailsActivity extends AppCompatActivity {
         tvRoomName.setText("Room Name: " + roomName);
         tvRoomCode.setText("Room Code: " + roomCode);
 
+        // Dummy student list
+        List<String> dummyStudents = new ArrayList<>();
+        dummyStudents.add("Juan Dela Cruz");
+        dummyStudents.add("Maria Clara");
+        dummyStudents.add("Jose Rizal");
+        dummyStudents.add("Andres Bonifacio");
+        dummyStudents.add("Emilio Aguinaldo");
 
+        // Setup RecyclerView
+        StudentAdapter adapter = new StudentAdapter(dummyStudents);
+        rvStudents.setLayoutManager(new LinearLayoutManager(this));
+        rvStudents.setAdapter(adapter);
+    }
 
+    public static class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
 
+        private List<String> students;
+
+        public StudentAdapter(List<String> students) {
+            this.students = students;
+        }
+
+        @NonNull
+        @Override
+        public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.teacher_essay_item_layout, parent, false);
+            return new StudentViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+            holder.tvStudentName.setText(students.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return students.size();
+        }
+
+        static class StudentViewHolder extends RecyclerView.ViewHolder {
+            TextView tvStudentName;
+
+            public StudentViewHolder(@NonNull View itemView) {
+                super(itemView);
+                tvStudentName = itemView.findViewById(R.id.text_student_name);
+            }
+        }
     }
 }
-
